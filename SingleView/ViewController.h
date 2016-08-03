6bf9205e8d094fe75bd2c20cf7a2a0c127bc89dc
zenghongyujia123 <1963968619@qq.com>
2016-08-03 16:19:09 +0800

    Initial Commit

---
+++ SingleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingleView
+//
+//  Created by zenghong on 16/8/3.
+//  Copyright © 2016年 zenghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SingleView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SingleView
+//
+//  Created by zenghong on 16/8/3.
+//  Copyright © 2016年 zenghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
